8e6d3d7f71f849b0eec0846a5bf0baa5f00eee99
benjaminbowen00 <benjibowen@hotmail.com>
2017-11-21 23:25:20 +0000

    comments on which player won. Some poor css added

--- controller.rb
+++ controller.rb
@@ -9,8 +9,9 @@ end
 
 
 get '/:hand1/:hand2' do
+
   game1 = Game.new(params[:hand1], params[:hand2])
   @game_result = game1.result
-
+  @game_statement = game1.game_statement
   erb( :result )
 end
--- models/game.rb
+++ models/game.rb
@@ -4,6 +4,7 @@ class Game
   def initialize(inp1, inp2)
     @inp1 = inp1.downcase
     @inp2 = inp2.downcase
+
   end
 
   def result
@@ -13,4 +14,26 @@ class Game
     return "Scissors wins" if arr == ["paper", "scissors"]
     return "It's a draw" if arr[0] == arr[1]
   end
+
+  def winning_info
+    arr = [@inp1, @inp2]
+    return {player: "Player 1", hand: @inp1 } if arr == ["rock", "scissors"]
+    return {player: "Player 2", hand: @inp2 } if arr == ["rock", "paper"]
+
+    return {player: "Player 1", hand: @inp1 } if arr == ["paper", "rock"]
+    return {player: "Player 2", hand: @inp2 } if arr == ["paper", "scissors"]
+
+    return {player: "Player 1", hand: @inp1 } if arr == ["scissors", "paper"]
+    return {player: "Player 2", hand: @inp2 } if arr == ["scissors", "rock"]
+
+    return {player: "Neither", hand: @inp1} if arr[0] == arr[1]
+
+  end
+
+  def game_statement
+  info = self.winning_info
+  return "Both players played #{info[:hand]} so the result is a draw " if info[:player] == "Neither"
+  return "#{info[:player]} won by playing #{info[:hand]}"
+  end
+
 end
